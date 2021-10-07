acd62649311e5e9c0abe4015bd40949fa4cf1586
Dat Tran <dat.tran@ghielectronics.com>
2021-10-07 13:20:34 -0400

    calculate temp in C

--- source/BrainPadTemperatureSensor.cpp
+++ source/BrainPadTemperatureSensor.cpp
@@ -7,5 +7,40 @@ BrainPadTemperatureSensor::BrainPadTemperatureSensor(BrainPadPin& pin, uint16_t 
 }
 
 int BrainPadTemperatureSensor::getValue() {
-    return (((codal::AnalogSensor::getValue() / 1024.0) * 3300.0) - 400.0) / 19.5;
+   
+	volatile uint32_t* ts_reg1 = (uint32_t*)0x1FFF75A8;
+	volatile uint32_t* ts_reg2 = (uint32_t*)0x1FFF75CA;
+	volatile uint32_t* enable_reg = (uint32_t*)(0x40000000U + 0x08000000U + 0x08040300U + 8);
+	
+	volatile uint32_t enable_val = *enable_reg;
+	volatile uint32_t ts1;
+	volatile uint32_t ts2; 
+	volatile double v;
+	volatile double t1;
+	volatile double t2;
+	volatile double t3;
+	volatile uint32_t value;
+
+	
+	if ((enable_val & 0x00800000) == 0) {
+		enable_val |= 0x00800000;
+		*enable_reg = enable_val;
+	}
+	
+	ts1 = *ts_reg1;
+	ts2 = *ts_reg2;
+	
+	v = codal::AnalogSensor::getValue() * 1.0;
+	
+	ts1 &= 0xFFFF;
+	ts2 &= 0xFFFF;
+	v *= 1.1;
+	
+	t1 = (110 - 30) * 1.0;
+	t2 = (ts2 - ts1) * 1.0;
+	t3 = (v - ts1) * 1.0;
+
+	value = t1 / t2 * t3 + 30;
+	
+	return value;
 }
